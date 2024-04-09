bb4f04bdf8c5344473557d93c1e7db8a97da1119
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-04-09 18:22:49 +0000

    add flash attention, but a naive implementation there of

--- dev/cuda/attention_forward.cu
+++ dev/cuda/attention_forward.cu
@@ -6,6 +6,13 @@ nvcc -O3 --use_fast_math attention_forward.cu -o attention_forward
 
 version 1 is naive port from CPU code to kernel, parallelize over batch, time, heads only
 ./attention_forward 1
+
+version 2 is a naive implementation of flash attention, taken, adapted from
+https://github.com/tspeterkim/flash-attention-minimal
+and with help from
+https://github.com/leloykun/flash-hyperbolic-attention-minimal
+sadly, this flash attention version seems about 3X slower than the naive version
+./attention_forward 2
 */
 
 #include <stdio.h>
@@ -201,6 +208,168 @@ __global__ void attention_value_kernel1(float* out, float* att, float* inp,
     }
 }
 
+__global__
+void attention_forward_kernel2(
+    const float* Q,
+    const float* K,
+    const float* V,
+    const int N,
+    const int d,
+    const int Tc,
+    const int Tr,
+    const int Bc,
+    const int Br,
+    const float softmax_scale,
+    float* l,
+    float* m,
+    float* O
+) {
+    int tx = threadIdx.x;
+    int bx = blockIdx.x; int by = blockIdx.y;  // batch and head index
+
+    // Offset into Q,K,V,O,l,m - different for each batch and head
+    int qkv_offset = (bx * gridDim.y * N * d) + (by * N * d);  // gridDim.y = nh
+    int lm_offset = (bx * gridDim.y * N) + (by * N);  // offset for l and m
+
+    // Define SRAM for Q,K,V,S
+    extern __shared__ float sram[];
+    int tile_size = Bc * d;  // size of Qi, Kj, Vj
+    float* Qi = sram;
+    float* Kj = &sram[tile_size];
+    float* Vj = &sram[tile_size * 2];
+    float* S = &sram[tile_size * 3];
+
+    for (int j = 0; j < Tc; j++) {
+
+        // Load Kj, Vj to SRAM
+        for (int x = 0; x < d; x++) {
+            Kj[(tx * d) + x] = K[qkv_offset + (tile_size * j) + (tx * d) + x];
+            Vj[(tx * d) + x] = V[qkv_offset + (tile_size * j) + (tx * d) + x];
+        }
+        __syncthreads();  // such that the inner loop can use the correct Kj, Vj
+
+        for (int i = 0; i < Tr; i++)  {
+            // if past the end of the sequence, break
+            if (i * Br + tx >= N) {
+                break;
+            }
+
+            // Load Qi to SRAM, l and m to registers
+            for (int x = 0; x < d; x++) {
+                Qi[(tx * d) + x] = Q[qkv_offset + (tile_size * i) + (tx * d) + x];
+            }
+            float row_m_prev = m[lm_offset + (Br * i) + tx];
+            float row_l_prev = l[lm_offset + (Br * i) + tx];
+
+            // S = QK^T, row_m = rowmax(S)
+            // S[tx][y] = Sum_{x = 0}^{d-1} {Qi[tx][x] * Kj[y][x]}
+            // row_m = Max_{y = 0}^{Bc-1} S[tx][y]
+            // with causal masking
+            float row_m = -INFINITY;
+            for (int y = 0; y < Bc; y++) {
+                if (j * Bc + y >= N) {
+                    break;
+                }
+                float sum = 0;
+                for (int x = 0; x < d; x++) {
+                    sum += Qi[(tx * d) + x] * Kj[(y * d) + x];
+                }
+                sum *= softmax_scale;
+                if (i * Br + tx < j * Bc + y)
+                    sum = -INFINITY;
+                S[(Bc * tx) + y] = sum;
+
+                if (sum > row_m)
+                    row_m = sum;
+            }
+
+            // implement softmax with causal masking
+            // P = exp(S - row_m), row_l = rowsum(P)
+            // P[tx][y] = exp(S[tx][y] - row_m)
+            float row_l = 0;
+            for (int y = 0; y < Bc; y++) {
+                if (j * Bc + y >= N) {
+                    break;
+                }
+                if (i * Br + tx < j * Bc + y)
+                    S[(Bc * tx) + y] = 0;
+                else
+                    S[(Bc * tx) + y] = __expf(S[(Bc * tx) + y] - row_m);
+                row_l += S[(Bc * tx) + y];
+            }
+
+            // Compute new m and l
+            float row_m_new = max(row_m_prev, row_m);
+            float row_l_new = (__expf(row_m_prev - row_m_new) * row_l_prev) + (__expf(row_m - row_m_new) * row_l);
+
+            // Write O, l, m to HBM
+            for (int x = 0; x < d; x++) {
+                float pv = 0;  // Pij * Vj
+                for (int y = 0; y < Bc; y++) {
+                    if (j * Bc + y >= N) {
+                        break;
+                    }
+                    pv += S[(Bc * tx) + y] * Vj[(y * d) + x];
+                }
+                O[qkv_offset + (tile_size * i) + (tx * d) + x] = (1 / row_l_new) \
+                    * ((row_l_prev * __expf(row_m_prev - row_m_new) * O[qkv_offset + (tile_size * i) + (tx * d) + x]) \
+                    + (__expf(row_m - row_m_new) * pv));
+            }
+            m[lm_offset + (Br * i) + tx] = row_m_new;
+            l[lm_offset + (Br * i) + tx] = row_l_new;
+        }
+        __syncthreads();  // otherwise, thread can use the wrong Kj, Vj in inner loop
+    }
+}
+
+__global__ void permute_kernel(float* q, float* k, float* v,
+                               const float* inp,
+                               int B, int N, int NH, int d) {
+    // okay so now, this kernel wants Q,K,V to all be of shape (B, NH, N, d)
+    // but instead, we have a single tensor QKV (inp) of shape (B, N, 3, NH, d)
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Q[b][nh_][n][d_] = inp[b][n][0][nh_][d_]
+
+    if (idx < B * NH * N * d) {
+        int b = idx / (NH * N * d);
+        int rest = idx % (NH * N * d);
+        int nh_ = rest / (N * d);
+        rest = rest % (N * d);
+        int n = rest / d;
+        int d_ = rest % d;
+
+        int inp_idx = \
+            (b * N * 3 * NH * d)
+            +   (n * 3 * NH * d)
+            +       (0 * NH * d)
+            +          (nh_ * d)
+            +                d_;
+
+        q[idx] = inp[inp_idx];
+        k[idx] = inp[inp_idx + NH * d];
+        v[idx] = inp[inp_idx + 2 * (NH * d)];
+    }
+}
+
+__global__ void unpermute_kernel(float* inp, float *out, int B, int N, int NH, int d) {
+   // out has shape (B, nh, N, d) but we need to unpermute it to (B, N, nh, d)
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // out[b][n][nh_][d_] <- inp[b][nh_][n][d_]
+    if (idx < B * NH * N * d) {
+        int b = idx / (NH * N * d);
+        int rest = idx % (NH * N * d);
+        int nh_ = rest / (N * d);
+        rest = rest % (N * d);
+        int n = rest / d;
+        int d_ = rest % d;
+
+        int other_idx = (b * NH * N * d) + (n * NH * d) + (nh_ * d) + d_;
+        out[other_idx] = inp[idx];
+    }
+}
+
 // ----------------------------------------------------------------------------
 // kernel launcher
 
@@ -219,6 +388,81 @@ void attention_forward1(float* out, float* preatt, float* att,
     attention_value_kernel1<<<num_blocks, block_size>>>(out, att, inp, B, T, C, NH);
 }
 
+
+void attention_forward2(float* out,
+                       float* inp,
+                       int B, int T, int C, int NH,
+                       const int block_size) {
+    // these are hardcoded to 32 for now
+    const int Bc = 32;
+    const int Br = 32;
+    // renaming these to be consistent with the kernel
+    // const int B = B;
+    const int nh = NH;
+    const int N = T;
+    const int d = C / NH;
+    // more
+    const int Tc = ceil((float) N / Bc);
+    const int Tr = ceil((float) N / Br);
+    const float softmax_scale = 1.0 / sqrt(d);
+    // create some temporary memory
+    float* l;
+    float* m;
+    cudaCheck(cudaMalloc(&l, B * nh * N * sizeof(float)));
+    cudaCheck(cudaMalloc(&m, B * nh * N * sizeof(float)));
+    cudaCheck(cudaMemset(l, 0, B * nh * N * sizeof(float)));
+    cudaCheck(cudaMemset(m, -10000.0f, B * nh * N * sizeof(float)));
+
+    // calculate SRAM size needed per block, ensure we have enough shared memory
+    int col_tile_size = Bc * d;  // size of Kj, Vj
+    int row_tile_size = Br * d;  // size of Qi
+    const int sram_size =
+        (2 * col_tile_size * sizeof(float))  // SRAM size for Kj, Vj
+        + (row_tile_size * sizeof(float))  // SRAM size for Qi
+        + (Bc * Br * sizeof(float));  // SRAM size for S
+    int max_sram_size;
+    cudaDeviceGetAttribute(&max_sram_size, cudaDevAttrMaxSharedMemoryPerBlock, 0);
+    if (sram_size > max_sram_size) {
+        printf("Max shared memory: %d, requested shared memory: %d \n", max_sram_size, sram_size);
+        printf("SRAM size exceeds maximum shared memory per block\n");
+        printf("Try decreasing col_tile_size or row_tile_size further\n");
+        exit(1);
+    }
+
+    // grid and block dims
+    dim3 grid_dim(B, nh);  // batch_size x num_heads
+    dim3 block_dim(Br);  // Br threads per block
+
+    // okay so now, this kernel wants Q,K,V to all be of shape (B, nh, N, d)
+    // but instead, we have a single tensor QKV (inp) of shape (B, N, 3, nh, d)
+    // so we have to permute the tensor using a kernel with block_size
+    float *q, *k, *v;
+    cudaCheck(cudaMalloc(&q, B * T * C * sizeof(float)));
+    cudaCheck(cudaMalloc(&k, B * T * C * sizeof(float)));
+    cudaCheck(cudaMalloc(&v, B * T * C * sizeof(float)));
+    int total_threads = B * N * nh * d;
+    int num_blocks = CEIL_DIV(total_threads, block_size);
+    permute_kernel<<<num_blocks, block_size>>>(q, k, v, inp, B, N, nh, d);
+
+    // now actually call the flash attention kernel
+    attention_forward_kernel2<<<grid_dim, block_dim, sram_size>>>(
+        q, k, v,
+        N, d, Tc, Tr, Bc, Br, softmax_scale,
+        l, m, out
+    );
+
+    // out has shape (B, nh, N, d) but we need to unpermute it to (B, N, nh, d)
+    unpermute_kernel<<<num_blocks, block_size>>>(out, q, B, N, nh, d);
+    cudaCheck(cudaMemcpy(out, q, B * T * C * sizeof(float), cudaMemcpyDeviceToDevice));
+
+    // free memory
+    cudaCheck(cudaFree(l));
+    cudaCheck(cudaFree(m));
+    cudaCheck(cudaFree(q));
+    cudaCheck(cudaFree(k));
+    cudaCheck(cudaFree(v));
+}
+
 // kernel version dispatch
 void attention_forward(int kernel_num,
                        float* out, float* preatt, float* att,
@@ -228,6 +472,9 @@ void attention_forward(int kernel_num,
     switch (kernel_num) {
         case 1:
             attention_forward1(out, preatt, att, inp, B, T, C, NH, block_size);
+            break;
+        case 2:
+            attention_forward2(out, inp, B, T, C, NH, block_size);
             break;
         default:
             printf("Invalid kernel number\n");
