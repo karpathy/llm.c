9666d4a814650987be127c6dc9ee2168727b148f
Ayush Anshul <ayushanshul07@gmail.com>
2024-04-11 07:42:31 +0000

    Fix repeated calculation on forward and back prop

--- train_gpt2.c
+++ train_gpt2.c
@@ -335,25 +335,24 @@ void attention_backward(float* dinp, float* dpreatt, float* datt,
     }
 }
 
+const float sqrt2ByPI = sqrtf(2.0f / M_PI);
 void gelu_forward(float* out, float* inp, int N) {
-    float s = sqrtf(2.0f / M_PI);
     for (int i = 0; i < N; i++) {
         float x = inp[i];
         float cube = 0.044715f * x * x * x;
-        out[i] = 0.5f * x * (1.0f + tanhf(s * (x + cube)));
+        out[i] = 0.5f * x * (1.0f + tanhf(sqrt2ByPI * (x + cube)));
     }
 }
 
 void gelu_backward(float* dinp, float* inp, float* dout, int N) {
-    float s = sqrtf(2.0f / M_PI);
     for (int i = 0; i < N; i++) {
         float x = inp[i];
         float cube = 0.044715f * x * x * x;
-        float tanh_arg = s * (x + cube);
+        float tanh_arg = sqrt2ByPI * (x + cube);
         float tanh_out = tanhf(tanh_arg);
         float coshf_out = coshf(tanh_arg);
         float sech_out = 1.0f / (coshf_out * coshf_out);
-        float local_grad = 0.5f * (1.0f + tanh_out) + x * 0.5f * sech_out * s * (1.0f + 3.0f * 0.044715f * x * x);
+        float local_grad = 0.5f * (1.0f + tanh_out) + x * 0.5f * sech_out * sqrt2ByPI * (1.0f + 3.0f * 0.044715f * x * x);
         dinp[i] += local_grad * dout[i];
     }
 }
