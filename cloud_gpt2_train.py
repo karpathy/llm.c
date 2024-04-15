701d8b25ffa5d61bae40b2af3eb998d86a7d2cbc
dongreenberg <dongreenberg2@gmail.com>
2024-04-15 10:59:28 -0400

    Add more comments.

--- cloud_gpt2_train.py
+++ cloud_gpt2_train.py
@@ -2,6 +2,7 @@ import runhouse as rh
 
 
 def train_cloud():
+    # This can be modified to use other instance types or other cloud providers.
     cluster = rh.cluster(name="rh-cuda-gpu", instance_type="A10G:1").up_if_not()
     env = rh.env(name="llm_c_train",
                  setup_cmds=[
@@ -10,7 +11,8 @@ def train_cloud():
                      "sudo apt-get update",
                      "sudo apt install clang -y --fix-missing"
                  ],
-                 working_dir="./")
+                 working_dir="./")  # rsync over the current git root
+    # Note this is cached, so it will not run the installation commands again unless they change.
     env.to(cluster)
     cluster.run(["cd llm.c; make train_gpt2",
                  "OMP_NUM_THREADS=8 llm.c/train_gpt2"])
