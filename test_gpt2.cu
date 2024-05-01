1ed9381fd06f456caf9a049abc8a4fabc67154e2
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-05-01 17:55:36 +0000

    fix cudnnDestroy outside of ifdef

--- test_gpt2.cu
+++ test_gpt2.cu
@@ -326,13 +326,10 @@ int main(int argc, char *argv[]) {
     free(grads_memory_cpu);
     free(grads_memory_cpu_float);
     gpt2_free(&model);
-
     #ifdef ENABLE_CUDNN
-    if (cudnn_workspace != NULL) {
-        cudaCheck(cudaFree(cudnn_workspace));
-    }
-    #endif
+    if (cudnn_workspace != NULL) { cudaCheck(cudaFree(cudnn_workspace)); }
     checkCudnnErr(cudnnDestroy(cudnn_handle));
+    #endif
     cudaCheck(cudaFree(cublaslt_workspace));
     cublasCheck(cublasDestroy(cublas_handle));
     cublasCheck(cublasLtDestroy(cublaslt_handle));
