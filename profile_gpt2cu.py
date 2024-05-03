13eeeb20c8f8c53e590aac1a12dae45c54b1712e
Erik Schultheis <erik.schultheis@aalto.fi>
2024-05-03 21:31:54 +0300

    try to detect if we need sudo

--- profile_gpt2cu.py
+++ profile_gpt2cu.py
@@ -17,10 +17,18 @@ if NCU is None:
 # build the executable
 subprocess.check_call(["make", "profile_gpt2cu", "NO_MULTI_GPU=1", "USE_CUDNN=1"])
 
+# try to see if profiling is allowed for non-root:
+options = subprocess.check_output(["modprobe", "-c", "nvidia"], text=True)
+can_profile = len([l for l in options.splitlines() if "NVreg_RestrictProfilingToAdminUsers=0" in l]) != 0
+
 # record metrics
 # --full and --import-source are entirely superfluous for this script, but you might want to
 # manually inspect `profile.ncu-rep`, so we keep it here
-cmd = ["sudo", NCU, "--set", "full", "--import-source", "yes", "-o", "profile", "-f", "./profile_gpt2cu"]
+cmd = [NCU, "--set", "full", "--import-source", "yes", "-o", "profile", "-f", "./profile_gpt2cu"]
+# do we need to run under sudo
+if not can_profile:
+    print("NVreg_RestrictProfilingToAdminUsers=1, running with sudo")
+    cmd = ["sudo"] + cmd
 subprocess.check_call(cmd)
 
 # generate csv
