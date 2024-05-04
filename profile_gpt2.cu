34304361bab643dbc80e24f200fb8aa257a659b7
ademeure <arundemeure@gmail.com>
2024-05-05 00:16:49 +0100

    fix merge conflict

--- profile_gpt2.cu
+++ profile_gpt2.cu
@@ -71,8 +71,8 @@ int main() {
     GPT2 model;
     gpt2_build_from_checkpoint(&model, "gpt2_124M_bf16.bin");
 
-    int B = 24;
-    int T = 1024;
+    int B = 24; // if program OOMs decrease this number, e.g. all the way down to 4 or etc
+    int T = 1024; // if even that OOMs move on to this one. keep them nice and powers of 2
     printf("batch size: %d\n", B);
     printf("sequence length: %d\n", T);
 
