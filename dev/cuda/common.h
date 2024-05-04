f0bf7b9787ed44407c52442e902409f3fdb5b73d
Erik Schultheis <erik.schultheis@aalto.fi>
2024-05-04 14:22:18 +0300

    macro-helpers to generate test/benchmark main for multiple-dtype testing

--- dev/cuda/common.h
+++ dev/cuda/common.h
@@ -56,7 +56,9 @@ int cuda_threads_per_SM = 0;    // needed to calculate how many blocks to launch
 
 template<class ElementType>
 struct alignas(16) Packed128 {
-    __device__ Packed128() = default;
+    // default gives implicit __device__.
+    // Making it explicit causes the compiler to emit warnings, so it is omitted here
+    Packed128() = default;
     __device__ explicit Packed128(int4 bits) {
         static_assert(sizeof(bits) == sizeof(payload), "Size mismatch.");
         memcpy(&payload, &bits, sizeof(bits));
@@ -168,36 +170,10 @@ template<class TargetType>
     return status;
 }
 
-void setup_main() {
-    srand(0);   // determinism
-
-    // set up the device
-    int deviceIdx = 0;
-    cudaCheck(cudaSetDevice(deviceIdx));
-    cudaDeviceProp deviceProp;
-    cudaGetDeviceProperties(&deviceProp, deviceIdx);
-    cuda_num_SMs = deviceProp.multiProcessorCount;
-    cuda_threads_per_SM = deviceProp.maxThreadsPerMultiProcessor;
-    cuda_arch_major = deviceProp.major;
-    cuda_arch_minor = deviceProp.minor;
-
-    // setup cuBLAS and cuBLASLt
-    cublasCheck(cublasCreate(&cublas_handle));
-    cublasCheck(cublasLtCreate(&cublaslt_handle));
-    cudaCheck(cudaMalloc(&cublaslt_workspace, cublaslt_workspace_size));
-
-    // TF32 precision is equivalent to torch.set_float32_matmul_precision('high')
-    int enable_tf32 = cuda_arch_major >= 8 ? 1 : 0;
-    // TODO implement common CLI for all tests/benchmarks
-    // if (override_enable_tf32 == 0) { enable_tf32 = 0; } // force to zero via arg
-    cublas_compute_type = enable_tf32 ? CUBLAS_COMPUTE_32F_FAST_TF32 : CUBLAS_COMPUTE_32F;
-    cublasMath_t cublas_math_mode = enable_tf32 ? CUBLAS_TF32_TENSOR_OP_MATH : CUBLAS_DEFAULT_MATH;
-    cublasCheck(cublasSetMathMode(cublas_handle, cublas_math_mode));
-}
-
 template<class D, class T>
-void validate_result(D* device_result, const T* cpu_reference, const char* name, std::size_t num_elements, T tolerance=1e-4) {
-    D* out_gpu = (D*)malloc(num_elements * sizeof(D));
+void validate_result(D* device_result, const T* cpu_reference, const char* name, std::size_t num_elements,
+                     T tolerance = 1e-4) {
+    D* out_gpu = (D*) malloc(num_elements * sizeof(D));
     cudaCheck(cudaMemcpy(out_gpu, device_result, num_elements * sizeof(D), cudaMemcpyDeviceToHost));
     int nfaults = 0;
     for (int i = 0; i < num_elements; i++) {
@@ -208,7 +184,7 @@ void validate_result(D* device_result, const T* cpu_reference, const char* name,
         // ensure correctness for all elements. We can set an "ignore" mask by writing NaN
         if (fabs(cpu_reference[i] - (T)out_gpu[i]) > tolerance && isfinite(cpu_reference[i])) {
             printf("Mismatch of %s at %d: CPU_ref: %f vs GPU: %f\n", name, i, cpu_reference[i], (T)out_gpu[i]);
-            nfaults ++;
+            nfaults++;
             if (nfaults >= 10) {
                 free(out_gpu);
                 exit(EXIT_FAILURE);
@@ -259,3 +235,132 @@ float benchmark_kernel(int repeats, Kernel kernel, KernelArgs&&... kernel_args) 
 
     return elapsed_time / repeats;
 }
+
+
+// ----------------------------------------------------------------------------
+// common test/benchmark main implementation
+
+// usage: each test/benchmark file needs to provide a kernel dispatch function, whose first argument is a kernel
+//        number, and which will call the correct kernel implementation by passing on all the other arguments.
+//        individual kernels should be implemented as templates over the floating-point types they operate on.
+//        Then, the macro DECLARE_TEST should be called, with the kernel dispatch function as its sole argument.
+//        Next, the IMPLEMENT_TEST macro prepares the implementation of the actual test/benchmark. It is used like
+//        a function declaration, that is, it should be called like `int IMPLEMENT_TEST(int kernel_num) {`.
+//        The `kernel_num` argument contains the requested kernel, and inside the curly braces, a `floatX`
+//        type is available that specifies the requested floating point type.
+// For an explanation how these macros do their magic, see below.
+
+// generic setup function that prepares the device and sets up cublas
+void setup_main() {
+    srand(0);   // determinism
+
+    // set up the device
+    int deviceIdx = 0;
+    cudaCheck(cudaSetDevice(deviceIdx));
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, deviceIdx);
+    cuda_num_SMs = deviceProp.multiProcessorCount;
+    cuda_threads_per_SM = deviceProp.maxThreadsPerMultiProcessor;
+    cuda_arch_major = deviceProp.major;
+    cuda_arch_minor = deviceProp.minor;
+
+    // setup cuBLAS and cuBLASLt
+    cublasCheck(cublasCreate(&cublas_handle));
+    cublasCheck(cublasLtCreate(&cublaslt_handle));
+    cudaCheck(cudaMalloc(&cublaslt_workspace, cublaslt_workspace_size));
+
+    // TF32 precision is equivalent to torch.set_float32_matmul_precision('high')
+    int enable_tf32 = cuda_arch_major >= 8 ? 1 : 0;
+    // TODO implement common CLI for all tests/benchmarks
+    // if (override_enable_tf32 == 0) { enable_tf32 = 0; } // force to zero via arg
+    cublas_compute_type = enable_tf32 ? CUBLAS_COMPUTE_32F_FAST_TF32 : CUBLAS_COMPUTE_32F;
+    cublasMath_t cublas_math_mode = enable_tf32 ? CUBLAS_TF32_TENSOR_OP_MATH : CUBLAS_DEFAULT_MATH;
+    cublasCheck(cublasSetMathMode(cublas_handle, cublas_math_mode));
+}
+
+// this defines the generic structure of the main function. This contains everything we can specify without
+// knowing the actual test, which is communicated through the TestCase class template.
+// Note: Since the TestCase itself has to be a template (needs to run on float, half, bfloat), we get the
+//       `template<class> class TestCase` syntax: TestCase is a class template, that takes a single class (floatX) as its
+//       template parameter.
+// You are not intended to invoke this function manually, nor to write a TestCase yourself; instead, this setup will
+// be handled by the macros below.
+template<template<class> class TestCase>
+int main_fn(int argc, const char** argv) {
+    setup_main();
+
+    // read kernel_num from command line
+    int kernel_num = 1;
+    if (argc > 1) {
+        kernel_num = atoi(argv[1]);
+    }
+    printf("Using kernel %d\n", kernel_num);
+
+    int dtype = 0;
+    if (argc > 2) {
+        if (strcmp(argv[2], "f32") == 0) {
+            dtype = 0;
+        } else if (strcmp(argv[2], "f16") == 0) {
+            dtype = 1;
+        } else if (strcmp(argv[2], "b16") == 0) {
+            dtype = 2;
+        } else {
+            fprintf(stderr, "Invalid dtype %s", argv[2]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    switch (dtype) {
+        case 0:
+            printf("Using float\n");
+            return TestCase<float>::run(kernel_num);
+        case 1:
+            printf("Using half\n");
+            return TestCase<half>::run(kernel_num);
+        case 2:
+            printf("Using bfloat16\n");
+            return TestCase<nv_bfloat16>::run(kernel_num);
+        default:
+            exit(EXIT_FAILURE);
+    }
+}
+
+// We have one more challenge to overcome: The kernel dispatch function has to be a template,
+// but that prevents us from passing it around as a parameter (cannot take the address of an overloaded function)
+// to the `benchmark_kernel` function above.
+// What we can pass around instead is a function object; but we don't want to require the test files to implement
+// these (which are a bit unnatural, and really just a workaround), so instead we have this macro that creates
+// a helper type `dispatcher_s` whose call operator is just forwarding to the original kernel dispatch.
+#define DECLARE_DISPATCHER(fptr) \
+struct dispatcher_s {            \
+    template<class... Args>         \
+    void operator()(Args&&... args) {   \
+        fptr(std::forward<Args>(args)...);  \
+    }   \
+}; \
+
+// This macro declares the TestCase class. It is templated over the floating-point type `floatX`, and defines a
+// run function to actually run the test.  It also provides a `dispatcher_s` variable of name `dispatcher` (which
+// is the name of the original dispatch function template). Therefore, inside this class, the dispatcher function
+// template is shadowed by the `dispatcher_s` object of the same name, and `benchmark_kernel` calls work seamlessly.
+#define DECLARE_TEST(dispatcher)    \
+DECLARE_DISPATCHER(dispatcher)      \
+template<class floatX>              \
+struct TestCase {                   \
+    using x128 = Packed128<floatX>; \
+    static int run(int kernel_num); \
+    static dispatcher_s dispatcher; \
+}
+
+
+// Finally, the actual test implementation. This macro is designed to have minimal syntax effect, to make it look as
+// close to a "regular" test function. Therefore, the implementation below does not specify `int main` but just main---
+// the `int` part is expected to be provided as part of the macro invocation in the test file. Similarly, we do not
+// auto-generate the `int kernel_num` parameter, but instead expect it to be given as "function arguments" to the macro.
+// Thus, in the test file, the only thing that "magically" appears is the floatX type.
+#define IMPLEMENT_TEST(...)                 \
+main(int argc, const char** argv) {     \
+    return main_fn<TestCase>(argc, argv);   \
+}                                           \
+template<class floatX>                      \
+int TestCase<floatX>::run(__VA_ARGS__)
