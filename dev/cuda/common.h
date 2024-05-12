b1f9742c3e5c33febd578d53fd3aa91f690d19e1
lancer <tangshao28@gmail.com>
2024-05-12 16:43:30 -0700

    Make ceil_div constexpr and inline

--- dev/cuda/common.h
+++ dev/cuda/common.h
@@ -6,7 +6,7 @@
 
 
 template<class T>
-__host__ __device__ T ceil_div(T dividend, T divisor) {
+constexpr __host__ __device__ inline T ceil_div(T dividend, T divisor) {
     return (dividend + divisor-1) / divisor;
 }
 
