7bd2389c1fe302b59f7ddc7dcacdb5f11e7765e2
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-05-19 14:23:51 +0000

    add mmlu as well and refine both a bit

--- dev/mmlu.py
+++ dev/mmlu.py
@@ -1,28 +1,16 @@
 """
-Downloads and evaluates HellaSwag in Python.
+Downloads and evaluates MMLU in Python.
 This then acts as the reference file for llm.c
-https://github.com/rowanz/hellaswag
+https://github.com/hendrycks/test
 
-Example HellaSwag json item:
-
-{"ind": 24, "activity_label": "Roof shingle removal", "ctx_a": "A man is sitting on a roof.", "ctx_b": "he", "ctx": "A man is sitting on a roof. he", "split": "val", "split_type": "indomain", "label": 3, "endings": ["is using wrap to wrap a pair of skis.", "is ripping level tiles off.", "is holding a rubik's cube.", "starts pulling up roofing on a roof."], "source_id": "activitynet~v_-JhWjGDPHMY"}
-
-ind: dataset ID
-activity_label: The ActivityNet or WikiHow label for this example
-context: There are two formats. The full context is in ctx. When the context ends in an (incomplete) noun phrase, like for ActivityNet, this incomplete noun phrase is in ctx_b, and the context up until then is in ctx_a. This can be useful for models such as BERT that need the last sentence to be complete. However, it's never required. If ctx_b is nonempty, then ctx is the same thing as ctx_a, followed by a space, then ctx_b.
-endings: a list of 4 endings. The correct index is given by label (0,1,2, or 3)
-split: train, val, or test.
-split_type: indomain if the activity label is seen during training, else zeroshot
-source_id: Which video or WikiHow article this example came from
-
-gpt2 (124M) = 28.2%
-gpt2-xl (1558M) = 39.22%
+gpt2 (124M) ~= 25% (chance)
+gpt2-xl (1558M) = 27.00% ...
 """
 
 import os
-import json
 import requests
 import tiktoken
+import pandas as pd
 from tqdm import tqdm
 
 import torch
@@ -31,15 +19,10 @@ from torch.nn import functional as F
 
 from transformers import GPT2LMHeadModel
 
-DATA_CACHE_DIR = os.path.join("data", "hellaswag")
-
-hellaswags = {
-    "train": "https://raw.githubusercontent.com/rowanz/hellaswag/master/data/hellaswag_train.jsonl",
-    "val": "https://raw.githubusercontent.com/rowanz/hellaswag/master/data/hellaswag_val.jsonl",
-    "test": "https://raw.githubusercontent.com/rowanz/hellaswag/master/data/hellaswag_test.jsonl",
-}
+DATA_CACHE_DIR = os.path.join("data", "mmlu")
 
 enc = tiktoken.get_encoding("gpt2")
+data_url = "https://people.eecs.berkeley.edu/~hendrycks/data.tar"
 
 def download_file(url: str, fname: str, chunk_size=1024):
     """Helper function to download a file from a given url"""
@@ -56,16 +39,36 @@ def download_file(url: str, fname: str, chunk_size=1024):
             size = file.write(data)
             bar.update(size)
 
-def download(split):
-    """Downloads HellaSwag DATA_CACHE_DIR"""
+def download():
+    """Downloads MMLU to DATA_CACHE_DIR"""
     os.makedirs(DATA_CACHE_DIR, exist_ok=True)
-    data_url = hellaswags[split]
-    data_filename = os.path.join(DATA_CACHE_DIR, f"hellaswag_{split}.jsonl")
+    data_filename = os.path.join(DATA_CACHE_DIR, f"data.tar")
     if not os.path.exists(data_filename):
         print(f"Downloading {data_url} to {data_filename}...")
         download_file(data_url, data_filename)
+        os.system(f"tar -xf {data_filename} -C {DATA_CACHE_DIR}") # untar
+        # creates a directory "data" inside it, with e.g. data/test/*csv
     else:
         print(f"{data_filename} already exists, skipping download...")
+
+def iterate_examples():
+    # there are 14,042 examples in total in the test set
+
+    download()
+    test_dir = os.path.join(DATA_CACHE_DIR, "data", "test")
+    csv_files = [f for f in os.listdir(test_dir) if f.endswith(".csv")]
+    for csv_file in csv_files:
+        csv_path = os.path.join(test_dir, csv_file)
+        print(csv_path)
+        df = pd.read_csv(csv_path, header=None)
+        n = df.shape[0]
+        for idx in range(n):
+            example = {
+                "question": df.iloc[idx, 0],
+                "endings": [df.iloc[idx, 1], df.iloc[idx, 2], df.iloc[idx, 3], df.iloc[idx, 4]],
+                "label": df.iloc[idx, 5],
+            }
+            yield example
 
 def render_example(example):
     """
@@ -74,16 +77,13 @@ def render_example(example):
     - mask (is 1 in the region of the candidate completion, where we evaluate likelihoods)
     - label (the index of the correct completion, which we hope has the highest likelihood)
     """
-    ctx = example["ctx"]
-    label = example["label"]
-    endings = example["endings"]
-
-    # gather up all the tokens
+    ctx = f"Question: {example['question']}\n\nAnswer:"
     ctx_tokens = enc.encode(ctx)
+
     tok_rows = []
     mask_rows = []
-    for end in endings:
-        end_tokens = enc.encode(" " + end) # note: prepending " " because GPT-2 tokenizer
+    for end in example["endings"]:
+        end_tokens = enc.encode(" " + str(end)) # note: prepending " " because GPT-2 tokenizer
         tok_rows.append(ctx_tokens + end_tokens)
         mask_rows.append([0]*len(ctx_tokens) + [1]*len(end_tokens))
 
@@ -95,16 +95,8 @@ def render_example(example):
         tokens[i, :len(tok_row)] = torch.tensor(tok_row)
         mask[i, :len(mask_row)] = torch.tensor(mask_row)
 
+    label = "ABCD".index(example["label"])
     return tokens, mask, label
-
-def iterate_examples(split):
-    # there are 10,042 examples in total in val
-
-    download(split)
-    with open(os.path.join(DATA_CACHE_DIR, f"hellaswag_{split}.jsonl"), "r") as f:
-        for line in f:
-            example = json.loads(line)
-            yield example
 
 @torch.no_grad()
 def evaluate(model_type, device):
@@ -114,7 +106,7 @@ def evaluate(model_type, device):
 
     num_correct = 0
     num_total = 0
-    for example in iterate_examples("val"):
+    for example in iterate_examples():
         tokens, mask, label = render_example(example)
         tokens = tokens.to(device)
         mask = mask.to(device)
@@ -146,10 +138,10 @@ def evaluate(model_type, device):
         num_correct += int(pred == label)
         print(f"accuracy: {num_correct/num_total:.4f} ({num_correct}/{num_total})")
 
-        # debug: pretty print a few examples, and the losses in each case
+        # debug prints
         if num_total < 10:
             print("---")
-            print(f"Context:\n {example['ctx']}")
+            print(f"Context:\n {example['question']}")
             print(f"Endings:")
             for i, end in enumerate(example["endings"]):
                 print(f"{i} (loss: {use_loss[i].item():.4f}) {end}")
