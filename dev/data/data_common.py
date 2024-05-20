71774b3c3d83f651e143f04cc2ab79b9b757f2eb
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-05-20 23:10:49 +0000

    oops forgot to include data_common.py

---
+++ dev/data/data_common.py
@@ -0,0 +1,21 @@
+"""
+Common utilities for the datasets
+"""
+
+import requests
+from tqdm import tqdm
+
+def download_file(url: str, fname: str, chunk_size=1024):
+    """Helper function to download a file from a given url"""
+    resp = requests.get(url, stream=True)
+    total = int(resp.headers.get("content-length", 0))
+    with open(fname, "wb") as file, tqdm(
+        desc=fname,
+        total=total,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=chunk_size):
+            size = file.write(data)
+            bar.update(size)
