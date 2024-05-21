4bbd01fe120e8b45343154de1e2b7d886c2b0c1e
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-05-21 00:16:07 +0000

    add fineweb, and add the first version of a new write_shard function that contains a header properly

--- dev/data/data_common.py
+++ dev/data/data_common.py
@@ -4,6 +4,8 @@ Common utilities for the datasets
 
 import requests
 from tqdm import tqdm
+import numpy as np
+
 
 def download_file(url: str, fname: str, chunk_size=1024):
     """Helper function to download a file from a given url"""
@@ -19,3 +21,23 @@ def download_file(url: str, fname: str, chunk_size=1024):
         for data in resp.iter_content(chunk_size=chunk_size):
             size = file.write(data)
             bar.update(size)
+
+
+def write_shard(filename, toks):
+    """Saves token data as a .bin file, for reading in C"""
+    assert len(toks) < 2**31, "token count too large" # ~2.1B tokens
+    # construct the header
+    header = np.zeros(256, dtype=np.int32)
+    header[0] = 20240520 # magic
+    header[1] = 1 # version
+    header[2] = len(toks) # number of tokens after the 256*4 bytes of header (each 2 bytes as uint16)
+    # validate that no token exceeds a uint16
+    maxtok = 2**16
+    assert all(0 <= t < maxtok for t in toks), "token dictionary too large for uint16"
+    # construct the tokens
+    toks_np = np.array(toks, dtype=np.uint16)
+    # write to file
+    print(f"writing {filename}")
+    with open(filename, "wb") as f:
+        f.write(header.tobytes())
+        f.write(toks_np.tobytes())
