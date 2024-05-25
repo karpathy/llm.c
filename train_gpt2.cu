05fb642ae1d920cedb2b425d61962e803744334b
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-05-25 23:49:16 +0000

    attempt fix zero 1

--- train_gpt2.cu
+++ train_gpt2.cu
@@ -2701,12 +2701,19 @@ void gpt2_multi_gpu_accumulate(GPT2* model, MultiGpuConfig* multi_gpu_config) {
 }
 
 float gpt2_update(GPT2 *model, float learning_rate, float beta1, float beta2, float eps, float weight_decay, float grad_clip, int t, MultiGpuConfig* multi_gpu_config) {
+    // update the model parameters using the AdamW optimizer
+    // keep in mind that optimizer sharding (ZeRO-1) assigns different parameters to different GPUs
+    // so we may not be responsible for the entire parameter tensor
+    // also, this function was very simple a while back but become very complex, only because we want to
+    // selectively weight decay some, but not all tensors :(
+    // TODO: revisit and probably refactor this entire function
     NVTX_RANGE_FN();
-    size_t shard_num_parameters = multi_gpu_config->shard_num_parameters;
-    size_t shard_offset = multi_gpu_config->shard_offset;
+    size_t shard_num_parameters = multi_gpu_config->shard_num_parameters; // num parameters we are responsible for
+    size_t shard_offset = multi_gpu_config->shard_offset; // offset into the full parameter tensor
     floatX* params_memory = (floatX*)model->params_memory;
     floatX* grads_memory = (floatX*)model->grads_memory;
 
+    // lazily allocate m,v memory and master weights (usually on the first iteration)
     if (model->m_memory == NULL) {
         printf0("allocating %zu MiB for AdamW optimizer state m\n", (shard_num_parameters * sizeof(float)) >> 20);
         printf0("allocating %zu MiB for AdamW optimizer state v\n", (shard_num_parameters * sizeof(float)) >> 20);
@@ -2717,7 +2724,8 @@ float gpt2_update(GPT2 *model, float learning_rate, float beta1, float beta2, fl
         if (model->use_master_weights == 1) {
             printf0("allocating %zu MiB for master copy of params\n", (shard_num_parameters * sizeof(float)) >> 20);
             cudaCheck(cudaMalloc((void**)&model->master_weights, shard_num_parameters * sizeof(float)));
-            copy_and_cast_kernel<<<CEIL_DIV(shard_num_parameters, 512), 512>>>(model->master_weights, params_memory + shard_offset, shard_num_parameters);
+            size_t grid_size = CEIL_DIV(shard_num_parameters, 512);
+            copy_and_cast_kernel<<<grid_size, 512>>>(model->master_weights, params_memory + shard_offset, shard_num_parameters);
             cudaCheck(cudaGetLastError());
         }
     }
@@ -2743,53 +2751,62 @@ float gpt2_update(GPT2 *model, float learning_rate, float beta1, float beta2, fl
     float beta1_correction = 1.0f - powf(beta1, t);
     float beta2_correction = 1.0f - powf(beta2, t);
     unsigned int seed = random_u32(&model->rng_state);
-
     // individually call the adamw_kernel3 on all parameter tensors separately
     size_t offset = 0;
     for (int i = 0; i < NUM_PARAMETER_TENSORS; i++) {
         size_t num_parameters = model->param_elements[i];
-        int num_blocks = CEIL_DIV(num_parameters, block_size);
-        // check if this part of the weight tensor is the responsibility of the current GPU:
-        // If the current weight ends after the beginning of my shard, and begins before the ending of my shard,
-        // I've got work to do
-        if(offset + num_parameters >= shard_offset && offset < shard_offset + shard_num_parameters) {
-            // we only want to weight decay the 2D tensors and leave all 1D tensors alone
-            // in particular this also decays the embedding weights, but this is ok:
-            // - the token embeddings are weight shared and participate in the final projection to logits
-            // - the position embeddings actively participate at every forward/backward pass
-            floatX* params_ptr = params_memory + offset;
-            floatX* grad_ptr = grads_memory + offset;
+        // the scope of this GPU's work is the range: [shard_offset, shard_offset + shard_num_parameters)
+        // this parameter's values are in the range:  [offset, offset + num_parameters)
+        // so we are responsible for some of its parameters if:
+        // 1) this parameter ends after we begin (i.e. offset + num_parameters > shard_offset)
+        // 2) this parameter begins before we end (i.e. offset < shard_offset + shard_num_parameters)
+        if(offset + num_parameters > shard_offset && offset < shard_offset + shard_num_parameters) {
+
+            // ok this tensor has at least one element inside the range of responsibility of this GPU
+            // let's figure out the exact span we wish to call the AdamW kernel on
+            floatX* params_ptr = NULL;
+            floatX* grad_ptr = NULL;
             float* m_ptr = NULL;
             float* v_ptr = NULL;
             float* master_ptr = NULL;
             size_t local_params = 0;
-            // another GPU is doing the beginning of this parameter, start in the middle
-            if(offset < shard_offset) {
+            // does the tensor begin before our responsibility?
+            if(offset <= shard_offset) {
+                // if so, our start point is exactly that of our responsibility, i.e. shard_offset
                 params_ptr = params_memory + shard_offset;
                 grad_ptr = grads_memory + shard_offset;
-                master_ptr = model->master_weights;
+                // note that (master_weights, m, v) are already only the "local slice" for this GPU,
+                // and are of size shard_num_parameters, instead of the total number of parameters
+                // so they do not get offset, i.e. we just start at their index 0
+                if (model->master_weights != NULL) { master_ptr = model->master_weights; }
                 m_ptr = model->m_memory;
                 v_ptr = model->v_memory;
-                // (shard_offset - offset) parameters have been handled by another  PU.
-                local_params = min(shard_num_parameters, offset + num_parameters - shard_offset);
+                // the number of parameters we have to update is the minimum of two ranges
+                local_params = min(shard_num_parameters, (offset + num_parameters) - shard_offset);
             } else {
+                // our start point is the location of this tensor, i.e. offset
                 params_ptr = params_memory + offset;
                 grad_ptr = grads_memory + offset;
-                if(model->master_weights)
-                    master_ptr = model->master_weights + (offset - shard_offset);
-                m_ptr = model->m_memory + (offset - shard_offset);
-                v_ptr = model->v_memory + (offset - shard_offset);
-                local_params = min(num_parameters, shard_num_parameters - (offset - shard_offset));
+                // this arithmetic gave me a headache but my little doodle example says it's right
+                size_t delta = offset - shard_offset;
+                if (model->master_weights != NULL) { master_ptr = model->master_weights + delta; }
+                m_ptr = model->m_memory + delta;
+                v_ptr = model->v_memory + delta;
+                local_params = min(num_parameters, shard_num_parameters - delta);
             }
-
+            // we only want to weight decay the 2D tensors and leave all 1D tensors alone
+            // in particular this also decays the embedding weights, but this is ok:
+            // - the token embeddings are weight shared and participate in the final projection to logits
+            // - the position embeddings actively participate at every forward/backward pass
             float wd = (i == 0 || i == 1 || i == 4 || i == 6 || i == 10 || i == 12) ? weight_decay : 0.0f;
+            // ok finally call the kernel
+            size_t num_blocks = CEIL_DIV(num_parameters, block_size);
             adamw_kernel3<<<num_blocks, block_size>>>(params_ptr, master_ptr, grad_ptr,
-                                                      m_ptr, v_ptr, local_params,
-                                                      learning_rate, beta1, beta2, beta1_correction, beta2_correction, eps, wd,
-                                                      grad_scale, seed);
+                                                      m_ptr, v_ptr, local_params, learning_rate,
+                                                      beta1, beta2, beta1_correction, beta2_correction,
+                                                      eps, wd, grad_scale, seed);
         }
-
-
+        // advance the offset pointer to the next parameter tensor
         offset += num_parameters;
     }
     cudaCheck(cudaGetLastError());
