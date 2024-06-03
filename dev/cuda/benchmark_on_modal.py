fbc1e4983cbd9b75b27fa962a923909f1b0bd1c0
vyom1611 <sharm843@umn.edu>
2024-06-03 17:00:10 +0530

    Benchmark modal script revamped - profiling and external libraries

--- dev/cuda/benchmark_on_modal.py
+++ dev/cuda/benchmark_on_modal.py
@@ -1,28 +1,23 @@
 """
 Script for running benchmarks on the Modal platform.
 This is useful for folks who do not have access to expensive GPUs locally.
-
 Example usage for cuda kernels:
 GPU_MEM=80 modal run benchmark_on_modal.py \
     --compile-command "nvcc -O3 --use_fast_math attention_forward.cu -o attention_forward -lcublas" \
     --run-command "./attention_forward 1"
-
 OR if you want to use cuDNN and openMPI etc.
 For training the gpt2 model with cuDNN use:
 GPU_MEM=80 modal run dev/cuda/benchmark_on_modal.py \
     --compile-command "make train_gpt2cu USE_CUDNN=1" \
     --run-command "./train_gpt2cu -i dev/data/tinyshakespeare/tiny_shakespeare_train.bin \
     -j dev/data/tinyshakespeare/tiny_shakespeare_val.bin -v 250 -s 250 -g 144 -f shakespeare.log -b 4"
-
 For profiling using nsight system:
 GPU_MEM=80 modal run dev/cuda/benchmark_on_modal.py \
     --compile-command "make train_gpt2cu USE_CUDNN=1" \
     --run-command "nsys profile \
     ./train_gpt2cu -i dev/data/tinyshakespeare/tiny_shakespeare_train.bin \
     -j dev/data/tinyshakespeare/tiny_shakespeare_val.bin -v 250 -s 250 -g 144 -f shakespeare.log -b 4"
-
 -> To profile the report using a GUI, download NVIDIA NSight System GUI version (this software can run on all OS, so you download it locally)
-
 NOTE: Currently there is a bug in the profiling using nsight system which produces a lot of errors on the command line but it
 does not actually interfere with the model training and validation. The report (that you download) is still generated and can be viewed from Nsight Systems
 """
@@ -33,13 +28,11 @@ import datetime
 
 import modal
 from modal import Image, Stub
-
 GPU_NAME_TO_MODAL_CLASS_MAP = {
     "H100": modal.gpu.H100,
     "A100": modal.gpu.A100,
     "A10G": modal.gpu.A10G,
 }
-
 N_GPUS = int(os.environ.get("N_GPUS", 1))
 GPU_MEM = int(os.environ.get("GPU_MEM", 40))
 GPU_NAME = os.environ.get("GPU_NAME", "A100")
