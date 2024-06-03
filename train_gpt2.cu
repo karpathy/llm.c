d697eaecfda7ca26f4dae2cac3ce09b7fe4f1490
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-03 23:12:20 +0000

    move the global norm kernel

--- train_gpt2.cu
+++ train_gpt2.cu
@@ -59,6 +59,8 @@ GPT-2 Transformer Neural Net training loop. See README.md for usage.
 #include "llmc/fused_classifier.cuh"
 // defines: adamw_kernel3
 #include "llmc/adamw.cuh"
+// defines: global_norm_squared
+#include "llmc/global_norm.cuh"
 // ----------- Multi-GPU support -----------
 #ifdef MULTI_GPU
 #include <mpi.h>
@@ -251,27 +253,6 @@ void set_zero_configs(MultiGpuConfig* multi_gpu_config, int zero_stage, size_t t
 // ----------------------------------------------------------------------------
 // Kernels
 
-template<class T>
-__global__ void global_norm_squared_kernel(float* out, const T* data, size_t count) {
-    // we want as few atomics as possible, so each block tries to do
-    // the maximum amount of work (so no fixed chunk, but instead iterating
-    // until we run out of data), and then we reduce inside the block
-    // and finally have just one atomic per block.
-    // out will be updated atomically from all thread blocks. It is a float, so the
-    // atomic op is unproblematic
-    size_t index = threadIdx.x + blockDim.x * blockIdx.x;
-    size_t grid_width = blockDim.x * gridDim.x;
-    float accumulator = 0.f;
-    for(size_t i = index; i < count; i += grid_width) {
-        accumulator += (float)data[i] * (float)data[i];
-    }
-    // warp-level reduce
-    float block_sum = blockReduce<warpReduceSum>(accumulator);
-    if(threadIdx.x == 0) {
-        atomicAdd(out, block_sum);
-    }
-}
-
 // device functions and the kernel to cast data between types
 template<typename Td, typename Ts>
 __device__ Td cast_value(Ts val);
@@ -298,25 +279,6 @@ __global__ void copy_and_cast_kernel(Td* dst, const Ts* src, size_t n) {
     if (idx < n) {
         dst[idx] = cast_value<Td, Ts>(src[idx]);
     }
-}
-
-// ----------------------------------------------------------------------------
-// kernel launchers
-
-template<typename T>
-void global_norm_squared(float* out, const T* values, size_t count) {
-    const int block_size = 512;
-    // launch just enough blocks to fill the grid. deliberately no DIV_CEIL.
-    // having one block less than possible is a tiny performance hit, having
-    // one block too many is catastrophic, since it only can start once all the other
-    // blocks finish. anyway, I think cuda_threads_per_SM should be a multiple of 512
-    // on all gpus, so the division really is going to be exact.
-    const int grid_size = deviceProp.maxThreadsPerMultiProcessor * deviceProp.multiProcessorCount / block_size;
-    assert(grid_size > 0);      // gives a better error than letting the call below fail
-    // initialize out with zero
-    cudaCheck(cudaMemset(out, 0, sizeof(float)));
-    global_norm_squared_kernel<<<grid_size, block_size>>>(out, values, count);
-    cudaCheck(cudaGetLastError());
 }
 
 // ----------------------------------------------------------------------------
