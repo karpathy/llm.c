ec92ff64a2376419c9e70fae022b9cedfbf81818
Aleksa Gordic <gordicaleksa@gmail.com>
2024-06-04 18:47:23 +0200

    Improve the design and comment

--- train_gpt2.py
+++ train_gpt2.py
@@ -697,9 +697,9 @@ if __name__ == "__main__":
         write_state(model, x, y, logits, loss, f"gpt2_{model_size_str}_debug_state.bin")
         # reset the train_loader for the optimization below
         train_loader.reset()
-        # clear the grads
-        for name, param in model.named_parameters():
-            param.grad = 0.0 * param.grad
+        # clear the grads here explicitly because otherwise we'd have a duplicate grad accumulation
+        # since in the training loop we do a backward() and then zero_grad() at the end of the loop
+        model.zero_grad()
 
     # -------------------------------------------------------------------------
     # main training loop
