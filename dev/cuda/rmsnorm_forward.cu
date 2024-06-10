b21873a91ab5193b39e7caa128528ad174277e78
Andre S <andre.slavescu@gmail.com>
2024-06-10 09:08:23 +0000

    wip for faster rms norm kernel

--- dev/cuda/rmsnorm_forward.cu
+++ dev/cuda/rmsnorm_forward.cu
@@ -26,11 +26,11 @@ void rmsnorm_forward_cpu(
             for (int i = 0; i < C; i++) {
                 sum_of_squares += x[i] * x[i];
             }
-            float rms_val = 1.0f / rsqrtf(sum_of_squares / C);
+            float rms_val = rsqrtf(sum_of_squares / C);
             // seek to the output position in out[b,t,:]
             float* out_bt = out + b * T * C + t * C;
             for (int i = 0; i < C; i++) {
-                float n = x[i] / rms_val; // normalized output
+                float n = x[i] * rms_val; // normalized output
                 float o = n * weight[i] + bias[i]; // scale and shift it
                 out_bt[i] = o; // write
             }
@@ -61,19 +61,21 @@ __global__ void rmsnorm_forward_kernel1(
         // Calculate the sum of squares
         float sum_of_squares = 0.0f;
 
+        #pragma unroll
         for (int i = 0; i < C; i++) {
             sum_of_squares += x[i] * x[i];
         }
 
         // Compute RMS value
         sum_of_squares = sum_of_squares / C;
-        float rms_val = 1.0f / rsqrtf(sum_of_squares);
+        float rms_val = rsqrtf(sum_of_squares);
 
         // Seek to the output position in out[idx,:]
         float* out_idx = out + idx * C;
 
+        #pragma unroll
         for (int i = 0; i < C; i++) {
-            float n = x[i] / rms_val; // Normalized output
+            float n = x[i] * rms_val; // Normalized output
             float o = n * weight[i] + bias[i]; // Scale and shift it
             out_idx[i] = o; // Write
         }
@@ -81,6 +83,88 @@ __global__ void rmsnorm_forward_kernel1(
         // Cache the RMS for the backward pass later
         rms[idx] = rms_val;
     }
+}
+
+// modified from: https://developer.nvidia.com/blog/faster-parallel-reductions-kepler/
+__inline__ __device__ int warp_all_reduce_sum(float val) {
+    constexpr int WARP_SIZE = 32;
+    constexpr unsigned FULL_MASK = 0xFFFFFFFF;
+
+    #pragma unroll
+    for (int stride = WARP_SIZE >> 1; stride > 0; stride >>= 1) {
+        val += __shfl_xor_sync(FULL_MASK, val, stride);
+    }
+
+    return val;
+}
+
+// modified from: https://developer.nvidia.com/blog/faster-parallel-reductions-kepler/
+__inline__ __device__ int block_all_reduce_sum(float val, int block_size) {
+    constexpr int WARP_SIZE = 32;
+
+    static __shared__ float shared[WARP_SIZE]; 
+    int tid = threadIdx.x;
+    int lane = tid % WARP_SIZE;
+    int warp_id = tid / WARP_SIZE;
+
+    val = warp_all_reduce_sum(val); // reduction across all warps
+    if (lane == 0) { shared[warp_id] = val; }; // write final value stored in threadId 0 to shared memory
+
+    __syncthreads();
+
+    val = (tid < block_size / WARP_SIZE) ? shared[lane] : 0.0f;
+    if (warp_id == 0) { val = warp_all_reduce_sum(val); } // warp-wise reduce into first warp
+    
+    return val;
+}
+
+__global__ void rms_val_kernel(
+    float* rms, 
+    const float* inp, 
+    int N, 
+    int C, 
+    int block_size
+) {
+    int tid = threadIdx.x;
+    int idx = blockIdx.x; // range [0, B*T)
+
+    const float* x = inp + idx * C;
+    float sum_of_squares = 0.0f;
+
+    // thread coarsening
+    #pragma unroll
+    for (int i = tid; i < C; i += block_size) {
+        sum_of_squares += x[i] * x[i];
+    }
+    sum_of_squares = block_all_reduce_sum(sum_of_squares, block_size);
+
+    // write the final result (at thread 0) to global memory
+    if (tid == 0) {
+        rms[idx] = rsqrtf(sum_of_squares / C);
+    }
+}
+
+__global__ void rmsnorm_forward_kernel2(
+    float* out, 
+    float* rms,
+    const float* inp, 
+    const float* weight, 
+    const float* bias,
+    int B,
+    int T, 
+    int C
+) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    int bt = idx / C;
+    int c = idx % C;
+
+    float rms_val = rms[bt];
+    float xi = inp[idx];
+    float n = xi * rms_val;
+    float o = n * weight[c] + bias[c];
+
+    out[idx] = o;
 }
 
 // ----------------------------------------------------------------------------
@@ -99,9 +183,32 @@ void rmsnorm_forward1(
 ) {
     const int N = B * T;
     const int grid_size = ceil_div(N, block_size);
-    rms_forward_kernel1<<<grid_size, block_size>>>(out, rms, inp, weight, bias, N, C);
+    rmsnorm_forward_kernel1<<<grid_size, block_size>>>(out, rms, inp, weight, bias, N, C);
     cudaCheck(cudaGetLastError());
 }
+
+void rmsnorm_forward2(
+    float* out, 
+    float* rms,
+    const float* inp, 
+    const float* weight, 
+    const float* bias,
+    int B, 
+    int T, 
+    int C,
+    const int block_size
+) {
+    int N = B * T;
+    // in rms, threads cooperate within blocks via reductions
+    rms_val_kernel<<<B * T, block_size, block_size * sizeof(float)>>>(rms, inp, N, C, block_size);
+    cudaCheck(cudaGetLastError());
+    // in the normalization, everything just gets flattened out
+    const int block_size2 = 256;
+    const int grid_size = ceil_div(B * T * C, block_size2);
+    rmsnorm_forward_kernel2<<<grid_size, block_size2>>>(out, rms, inp, weight, bias, B, T, C);
+    cudaCheck(cudaGetLastError());
+}
+
 
 // kernel version dispatch
 void rmsnorm_forward(
@@ -119,6 +226,9 @@ void rmsnorm_forward(
     switch (kernel_num) {
         case 1:
             rmsnorm_forward1(out, rms, inp, weight, bias, B, T, C, block_size);
+            break;
+        case 2:
+            rmsnorm_forward2(out, rms, inp, weight, bias, B, T, C, block_size);
             break;
         default:
             printf("Invalid kernel number\n");
@@ -161,7 +271,7 @@ int main(int argc, char **argv) {
     cudaCheck(cudaMemcpy(d_bias, bias, C * sizeof(float), cudaMemcpyHostToDevice));
 
     // read kernel_num from command line
-    int kernel_num = 1;
+    int kernel_num = 2;
     if (argc > 1) {
         kernel_num = atoi(argv[1]);
     }
@@ -178,7 +288,7 @@ int main(int argc, char **argv) {
         int block_size = block_sizes[j];
         printf("Checking block size %d.\n", block_size);
 
-        layernorm_forward(kernel_num, d_out, d_rms, d_inp, d_weight, d_bias, B, T, C, block_size);
+        rmsnorm_forward(kernel_num, d_out, d_rms, d_inp, d_weight, d_bias, B, T, C, block_size);
 
         validate_result(d_out, out, "out", B * T * C, 1e-5f);
         validate_result(d_rms, rms, "rms", B * T, 1e-5f);
@@ -193,7 +303,7 @@ int main(int argc, char **argv) {
         int repeat_times = 2000;
         float elapsed_time = benchmark_kernel(
                                 repeat_times, 
-                                layernorm_forward,
+                                rmsnorm_forward,
                                 kernel_num, 
                                 d_out, 
                                 d_rms, 
