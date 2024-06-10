09fed1a3cf57980b9b0d6f199d99918bcb90d99f
maveriq <harisjabbar@gmail.com>
2024-06-11 00:18:27 +0200

    added get_hf_model which gives an HF model from the binary file.

--- test_read_checkpoint.py
+++ test_read_checkpoint.py
@@ -1,5 +1,5 @@
 from train_gpt2 import GPTConfig, GPT, write_model
-from read_checkpoint import get_state
+from read_checkpoint import get_state, get_hf_model
 import torch
 import tempfile
 import pytest
@@ -28,10 +28,28 @@ def test_small_model(dtype):
     run_model_test(model,dtype)
 
 @pytest.mark.parametrize("model_id", ["gpt2"]) # to add future model tests      
-def test_hf_model(model_id):
+def test_config_model(model_id):
     model = GPT.from_pretrained(model_id)
     dtype = 'float32' # 'float32'
     run_model_test(model,dtype)
+
+@pytest.mark.parametrize("model_id", ["gpt2"]) # to add future model tests     
+def test_hf_model(model_id):
+    model = GPT.from_pretrained(model_id)
+    dtype = 'float32' # 'float32'
+
+    tmp_file_name = '/tmp/model.bin'
+    write_model(model, tmp_file_name, dtype=dtype)
         
+    model_hf = get_hf_model(tmp_file_name)
 
+    transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+    model_dict = dict(model.named_parameters())
+    for n,p1 in model_hf.named_parameters(): # This ensures that we loop over all parameters from the HF model
+        p2 = model_dict.pop(n) # This removes all common parameters
 
+        if any(n.endswith(w) for w in transposed): # Transposing the weight matrices for certain parameters
+            assert torch.equal(p1,p2.t())
+        else:
+            assert torch.equal(p1,p2)
+    assert len(model_dict)==0 # All parameters should have been popped; thus proving that the two models are identical
