1e7c31eef0db8568422807c1c9a9efb49011cbc6
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-15 16:36:43 +0000

    add test for shuffled, permuted, multi-process dataloader

--- dev/test/test_dataloader.c
+++ dev/test/test_dataloader.c
@@ -12,30 +12,38 @@ char shard_name[64];
 int num_tokens = 140;
 int num_shards = 4;
 
-void checkRange(const int *tokens, const int start, const int end) {
+void check_range(const int *tokens, const int start, const int end, const char *file, int line) {
     // checks that the tokens[0, ... end-start] are the range [start, end)
     int n = end - start;
     for (int i = 0; i < n; i++) {
         int token = tokens[i];
         if (token != start + i) {
-            printf("Error: tokens[%d] = %d, expected %d\n", i, token, start + i);
+            fprintf(stderr, "Error: tokens[%d] = %d, expected %d\n", i, token, start + i);
+            fprintf(stderr, "Error details:\n");
+            fprintf(stderr, "  File: %s\n", file);
+            fprintf(stderr, "  Line: %d\n", line);
             exit(EXIT_FAILURE);
         }
     }
     // printf("tokens in range [%d, %d) OK\n", start, end);
 }
+#define checkRange(tokens, start, end) check_range(tokens, start, end, __FILE__, __LINE__)
 
-void checkEquals(const int *tokens, const int n, const int expected) {
+void check_equals(const int *tokens, const int n, const int expected, const char *file, int line) {
     // checks that the tokens[0, ... n] are all equal to expected
     for (int i = 0; i < n; i++) {
         int token = tokens[i];
         if (token != expected) {
-            printf("Error: tokens[%d] = %d, expected %d\n", i, token, expected);
+            fprintf(stderr, "Error: tokens[%d] = %d, expected %d\n", i, token, expected);
+            fprintf(stderr, "Error details:\n");
+            fprintf(stderr, "  File: %s\n", file);
+            fprintf(stderr, "  Line: %d\n", line);
             exit(EXIT_FAILURE);
         }
     }
     // printf("tokens all equal to %d OK\n", expected);
 }
+#define checkEquals(tokens, n, expected) check_equals(tokens, n, expected, __FILE__, __LINE__)
 
 void test_simple(void) {
     /*
@@ -179,6 +187,80 @@ void test_shuffled(void) {
     printf("OK\n");
 }
 
+void test_multiprocess_shuffled(void) {
+    /*
+    Tests the DataLoader when using both multiprocess and shuffled:
+    - multi-shard
+    - multi-process
+    - shuffled!
+    DataLoaders should return all the tokens, but in randperm order.
+    So all we check is that we see all the tokens we expect to see,
+    the correct number of times, over multiple epochs.
+    */
+
+    printf("test_multiprocess_shuffled... ");
+    int B = 4;
+    int T = 8;
+    int num_processes = 2;
+    int should_shuffle = 0;
+    snprintf(shard_name, 64, "shard_????.bin");
+    DataLoader loaders[num_processes];
+    for (int i = 0; i < num_processes; i++) {
+        dataloader_init(&loaders[i], shard_name, B, T, i, num_processes, should_shuffle);
+    }
+
+    // get batches from the dataloader and keep stats on what tokens we see
+    int total_tokens = num_shards * num_tokens;
+    int *num_seen_inputs = (int *)calloc(total_tokens, sizeof(int));
+    int *num_seen_targets = (int *)calloc(total_tokens, sizeof(int));
+    int batches_fit = num_tokens / (B * T * num_processes); // number of batches that fit per shard
+    int BT = B * T;
+    int num_epochs = 4;
+    for (int e = 0; e < num_epochs; e ++) { // epoch
+        for (int s = 0; s < num_shards; s++) { // shard
+            int start = s * num_tokens;
+            for (int b = 0; b < batches_fit; b++) { // batch
+                for (int n = 0; n < num_processes; n++) { // dataloader
+                    DataLoader *loader = &loaders[n];
+                    dataloader_next_batch(loader);
+                    // count up the tokens we see
+                    for (int i = 0; i < BT; i++) {
+                        int input_token = loader->inputs[i];
+                        int target_token = loader->targets[i];
+                        assert(input_token >= 0 && input_token < total_tokens);
+                        assert(target_token >= 0 && target_token < total_tokens);
+                        num_seen_inputs[input_token]++;
+                        num_seen_targets[target_token]++;
+                    }
+                    start += BT;
+                }
+            }
+        }
+    }
+
+    // verify that we saw all the tokens the correct number of times
+    int tokens_fit = batches_fit * (B * T * num_processes); // number of tokens that fit per shard
+    for (int s = 0; s < num_shards; s++) {
+        int start = s * num_tokens; // token id that starts this shard
+        // verify the inputs counts for this shard:
+        // - the first tokens_fit should have been seen num_epochs times
+        // - the rest of the tokens in that should should have been seen zero times
+        checkEquals(num_seen_inputs + start, tokens_fit, num_epochs);
+        checkEquals(num_seen_inputs + start + tokens_fit, num_tokens - tokens_fit, 0);
+        // verify the target counts. same thing but offset by 1
+        checkEquals(num_seen_targets + start + 1, tokens_fit, num_epochs);
+        checkEquals(num_seen_targets + start + 1 + tokens_fit, num_tokens - tokens_fit, 0);
+    }
+
+    // cleanup
+    for (int i = 0; i < num_processes; i++) {
+        dataloader_free(&loaders[i]);
+    }
+    free(num_seen_inputs);
+    free(num_seen_targets);
+    printf("OK\n");
+}
+
 int main(void) {
 
     // generate a few dummy shards of data with incrementing tokens
@@ -205,6 +287,7 @@ int main(void) {
     test_simple();
     test_multiprocess_simple();
     test_shuffled();
+    test_multiprocess_shuffled();
 
     // clean up the shards
     for (int shard_id = 0; shard_id < num_shards; shard_id++) {
