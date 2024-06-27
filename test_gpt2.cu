68f37349912c67eb7c103cffdc227e2597d94ad8
ademeure <arundemeure@gmail.com>
2024-06-27 16:27:36 +0000

    Make test_gpt2cu "-rg" setting behave the same way as in train_gpt2cu

--- test_gpt2.cu
+++ test_gpt2.cu
@@ -121,7 +121,7 @@ int main(int argc, char *argv[]) {
         if (argv[i][0] != '-') { exit(EXIT_FAILURE); } // must start with dash
         if (argv[i][1] == 'w') { model.use_master_weights = atoi(argv[i+1]); }
         else if (argv[i][1] == 'r' && argv[i][2] == '\0') { model.recompute = atoi(argv[i+1]); }
-        else if (argv[i][1] == 'r' && argv[i][2] == 'g') { model.rng_state += atoi(argv[i+1]); }
+        else if (argv[i][1] == 'r' && argv[i][2] == 'g') { model.rng_state = atoi(argv[i+1]) + multi_gpu_config.process_rank; }
     }
 
     // load additional information that we will use for debugging and error checking
