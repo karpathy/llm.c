717fc5a3877a092c3697ea63a333664e86aaac43
Aleksa Gordic <gordicaleksa@gmail.com>
2024-06-29 22:06:19 +0200

    Clear sum_d to 0 before collecting l1 stats

--- llmc/encoder.cuh
+++ llmc/encoder.cuh
@@ -168,6 +168,7 @@ void encoder_forward(floatX* out,
         double sum = 0.0;
         double* sum_d;
         cudaMalloc(&sum_d, sizeof(double));
+        cudaCheck(cudaMemsetAsync(sum_d, 0, sizeof(double), stream));
         abs_sum_kernel<<<B*T, WARP_SIZE, 0, stream>>>(out, B*T, C, sum_d);
         cudaCheck(cudaGetLastError());
         cudaCheck(cudaMemcpy(&sum, sum_d, sizeof(double), cudaMemcpyDeviceToHost));
